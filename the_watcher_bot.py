73272e51a8d6afe508c00cc7bea1051057f727f9
Jordan Brooks <jordbrooks14@gmail.com>
2017-12-04 20:47:44 -0800

    Add list to keep track of comments replied to.

--- the_watcher_bot.py
+++ the_watcher_bot.py
@@ -1,3 +1,4 @@
+import os
 import praw
 import time
 
@@ -6,7 +7,7 @@ REPLY_MESSAGE = "Thanks for calling me!"
 def authenticate():
     print("Authenticating...")
     reddit = praw.Reddit(
-        'dogbot',
+        'the_watcher_bot',
         user_agent="the_watcher_bot v0.1")
     print("Authenticated as {}".format(reddit.user.me()))
     return reddit
@@ -17,11 +18,25 @@ def main():
         run_bot(reddit)
 
 def run_bot(reddit):
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = f.read()
+            comments_replied_to = comments_replied_to.split("\n")
+            comments_replied_to = list(filter(None, comments_replied_to))
+
     print("Obtaining comments...")
-    for comment in reddit.subreddit("test").comments(limit=25):
-        if "The Watcher Bot" in comment.body:
+    subreddit = reddit.subreddit("test")
+    for comment in subreddit.comments(limit=25):
+        if "The Watcher Bot:" in comment.body and comment.id not in comments_replied_to:
             print("String with keyword found in comment {}".format(comment.id))
             comment.reply(REPLY_MESSAGE)
+            comments_replied_to.append(comment.id)
+            
+    with open("comments_replied_to.txt", "w") as f:
+        for comment_id in comments_replied_to:
+            f.write(comment_id + "\n")
 
     print("Sleeping...")
     time.sleep(10)
